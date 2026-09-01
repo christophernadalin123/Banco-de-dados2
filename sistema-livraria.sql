b9e2573e791feb5a7c39343321e521bfc1e0a2ad
christophernadalin123 <christopher.nadalin@escola.pr.gov.br>
2026-09-01 17:48:53 -0300

    Add sample data for Clientes and Compras tables
    Inserted sample data into Clientes and Compras tables.

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -12,3 +12,22 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(150) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+===================================================================
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (nomeCliente, emailCliente)
+VALUES 
+('Ana Silva', 'ana.silva@email.com'),
+('Bruno Costa', 'bruno.costa@email.com'),
+('Carla Souza', 'carla.souza@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (ClienteID, NomeLivro)
+VALUES
+(1, 'Dom Casmurro'),
+(2, 'O Senhor dos Anéis'),
+(3, '1984'),
+(1, 'A Hora da Estrela');
+
+SELECT * FROM Clientes; 
+SELECT * FROM Compras; 
